9c6856535a19c4f46baaea7cc740ad9ddaaff979
by-luckk <by-chen22@mails.tsinghua.edu.cn>
2025-05-29 13:01:35 +0800

    deal with conflict

--- element/Material.py
+++ element/Material.py
@@ -53,8 +53,8 @@ class CBarMaterial(CMaterial):
 						 "\n   Provided set : {}".format(mset + 1, self.nset)
 			raise ValueError(error_info)
 
-		self.E = np.double(line[1])
-		self.Area = np.double(line[2])
+		self.E = float(line[1])
+		self.Area = float(line[2])
 
 	def Write(self, output_file):
 		"""
@@ -86,9 +86,9 @@ class CQ4Material(CMaterial):
                          "\n   Provided set : {}".format(mset + 1, self.nset)
             raise ValueError(error_info)
 
-        self.E = np.double(line[1])
-        self.nu = np.double(line[2])
-        self.thickness = np.double(line[3])
+        self.E = float(line[1])
+        self.nu = float(line[2])
+        self.thickness = float(line[3])
         if len(line) > 4:
             self.plane_stress = bool(int(line[4]))
 
@@ -119,8 +119,8 @@ class CH8Material(CMaterial):
                           f"\n   Provided set : {self.nset}")
             raise ValueError(error_info)
 
-        self.E = np.double(line[1])
-        self.nu = np.double(line[2]) # Poisson's ratio
+        self.E = float(line[1])
+        self.nu = float(line[2]) # Poisson's ratio
 
     def Write(self, output_file):
         """
@@ -175,3 +175,64 @@ class CT3Material(CMaterial):
         material_info = f"{self.nset:5d}{self.E:16.6e}{self.nu:16.6e}{self.thickness:16.6e}\n"
         print(material_info, end="")           # console echo
         output_file.write(material_info)
+
+class CPlateMaterial(CMaterial):
+    """ Material class for plate element """
+    def __init__(self):
+        super().__init__()
+        self.thickness = 0.0      # 板厚度
+        self.nu = 0.0            # 泊松比
+        self.density = 0.0        # 材料密度(可选)
+        self.shear_correction = 5.0/6.0  # 剪切修正系数(默认5/6)
+
+    def Read(self, input_file, mset):
+        """
+        Read plate material data from input file
+        输入格式: nset E nu thickness [density] [shear_correction]
+        """
+        line = input_file.readline().split()
+
+        self.nset = np.int_(line[0])
+        if self.nset != mset + 1:
+            error_info = "\n*** Error *** Material sets must be inputted in order !" \
+                         "\n   Expected set : {}" \
+                         "\n   Provided set : {}".format(mset + 1, self.nset)
+            raise ValueError(error_info)
+
+        # 读取必要参数
+        self.E = float(line[1])
+        self.nu = float(line[2])
+        self.thickness = float(line[3])
+        
+        # 读取可选参数
+        if len(line) > 4:
+            self.density = float(line[4])
+        if len(line) > 5:
+            self.shear_correction = float(line[5])
+
+    def Write(self, output_file):
+        """
+        Write plate material data to output file
+        输出格式: nset E nu thickness density shear_correction
+        """
+        material_info = "%5d%16.6e%16.6e%16.6e%16.6e%16.6e\n" % (
+            self.nset, 
+            self.E, 
+            self.nu, 
+            self.thickness,
+            self.density,
+            self.shear_correction
+        )
+        
+        # print the material info on the screen
+        print(material_info, end='')
+        # write the material info to output file
+        output_file.write(material_info)
+
+    def GetBendingRigidity(self):
+        """ 计算板的弯曲刚度矩阵 """
+        return (self.E * self.thickness**3) / (12 * (1 - self.nu**2))
+
+    def GetShearModulus(self):
+        """ 计算剪切模量 """
+        return self.E / (2 * (1 + self.nu))
